75912691345d280fed2696791ef19694f86b8a67
Robert <67438520+freshskates@users.noreply.github.com>
2020-07-05 21:09:36 -0700

    updated

--- nsaEncoder1/nsaEncoder1.cpp
+++ nsaEncoder1/nsaEncoder1.cpp
@@ -1,44 +1,32 @@
-//libraries
+
 #include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
 
-//Programmer defined data types
-//NONE
+string getFileName(); 
 
-//Special compiler dependent definitions
-//NONE
 
-//global constants/variables
-//NONE
-
-//Programmer defined functions
-void introduction(string obj, string ins); //user introduction
-string getFileName(); //get user filename
-
-//main program
 int main()
 {
-    //Data
-    string objective = "Encode any string entered by adding 1 to the ASCII value, outputs to secret.txt"; //program objective
-    string instructions = ""; //user instructions if needed
-    string fileName; //user selected input filename
-    ifstream fin; //represents input file
+    
+    string objective = "Encode any string entered by adding 1 to the ASCII value, outputs to secret.txt"; 
+    string instructions = ""; 
+    string fileName; 
+    ifstream fin; 
     ofstream fout;
-    string line; //one line from input file
-    int lineNumber = 0; //number of lines in the input file
+    string line; 
+    int lineNumber = 0; 
     string output = "secret.txt";
-    introduction(objective, instructions); //user introduction
 
-    //input and open user selected file name
+   
 
     fileName = getFileName();
     fin.open(fileName.c_str());
     fout.open(output.c_str());
     if (!(fin.good())) throw "I/O error";
 
-    //read and output each line in the input file
+
     while (fin.good())
     {
         lineNumber = lineNumber + 1;
@@ -53,39 +41,22 @@ int main()
 
         fout << line << endl;
         cout << line << endl;
-    }//while fin.good
+    }
 
-}//main
+}
 
-// introduction
-void introduction(string obj, string ins)
-{
-    //data
-    //obj is the program objective from main program
-    //ins is the optional user instructions
 
-    //output user introduction
-    cout << "Objective: This program will ";
-    cout << obj << endl;
-    cout << "Programmer: Teacher\n";
-    cout << "Editor(s) used: Notepad\n";
-    cout << "Compiler(s) used: TDM MinGW\n";
-    cout << "File: " << __FILE__ << endl;
-    cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl;
-    cout << endl << ins << endl << endl;
-}//introduction
 
-//get user specified filename
 string getFileName()
 {
-    //data
-    string fN; //user specified filename
 
-    //input filename
+    string fN;
+
+
     do
     {
         cout << "Please enter an input filename [i.e.: x.txt]: ";
         getline(cin, fN);
-    } while (!(fN.length() >= 5)); //do-while
+    } while (!(fN.length() >= 5));
     return fN;
-}//getFileName
+}
